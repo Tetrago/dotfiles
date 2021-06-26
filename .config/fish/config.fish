a8a02332171a027ec9a9824b5d6339c07f6fc3c8
Tetrago <jamestgeiss@gmail.com>
2021-06-25 23:18:37 -0400

    Initial commit.

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+set -x EDITOR vim
+
+alias ps="procs"
+alias cat="bat"
+alias ls="exa --oneline"
+alias la="exa --oneline --all"
+alias ll="exa --oneline --all --long"
+alias mv="mv -i"
+alias cp="cp -i"
+alias clip="xclip -selection clipboard"
+
+neofetch
