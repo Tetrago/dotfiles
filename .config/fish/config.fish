063213e3d968b88a747fd61e614a17f268d1e9f0
James <jamestgeiss@gmail.com>
2021-06-28 20:12:11 -0400

    Repository split and configuration update.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,12 @@ set -x EDITOR vim
 
 alias ps="procs"
 alias cat="bat"
-alias ls="exa --oneline"
-alias la="exa --oneline --all"
-alias ll="exa --oneline --all --long"
+alias la="exa --1la --group-directories-first --color=always"
 alias mv="mv -i"
 alias cp="cp -i"
 alias tar="dtrx"
+alias dotfiles="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 
 colorscript random | tail -n +2
+
+starship init fish | source
