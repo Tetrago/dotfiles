83db28a9fca98f18afd42b01b0f94902471edbeb
James <jamestgeiss@gmail.com>
2021-07-28 23:45:59 -0400

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,23 @@
 set fish_greeting
 set -x EDITOR vim
 
-alias ps="procs"
-alias cat="bat"
-alias ls="exa -1la --group-directories-first --color=always"
-alias la="exa -1a --group-directories-first --color=always"
-alias ll="exa -1l --group-directories-first --color=always"
 alias mv="mv -i"
 alias cp="cp -i"
 alias dotfiles="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+
+if type -q procs
+  alias ps="procs"
+end
+
+if type -q bat
+  alias cat="bat"
+end
+
+if type -q exa
+  alias ls="exa -1la --group-directories-first --color=always"
+  alias la="exa -1a --group-directories-first --color=always"
+  alias ll="exa -1l --group-directories-first --color=always"
+end
 
 if type -q pcmanfm
   alias op="pcmanfm"
