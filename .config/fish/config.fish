beac84956dc60a023515e63c47ec930be4a0a18c
James <jamestgeiss@gmail.com>
2021-09-22 09:36:31 -0400

    Switched to xdg-open.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ set -x EDITOR vim
 alias mv="mv -i"
 alias cp="cp -i"
 alias dotfiles="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+alias op="xdg-open"
 
 if type -q procs
   alias ps="procs"
@@ -17,10 +18,6 @@ if type -q exa
   alias ls="exa -la --group-directories-first --color=always"
   alias la="exa -1a --group-directories-first --color=always"
   alias ll="exa -l --group-directories-first --color=always"
-end
-
-if type -q pcmanfm
-  alias op="pcmanfm"
 end
 
 if type -q neofetch
