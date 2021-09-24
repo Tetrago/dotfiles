ba049f286a3782cab1e2e2c2bf68d7c414f33445
James <jamestgeiss@gmail.com>
2021-09-24 10:02:55 -0400

    bat manpager.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 set fish_greeting
 set -x EDITOR vim
 
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
 alias mv="mv -i"
 alias cp="cp -i"
 alias dotfiles="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
